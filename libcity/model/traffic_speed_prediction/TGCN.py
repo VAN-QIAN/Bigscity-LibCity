46d0d4574f285194a4ea41edb0ac6026fd0edf48
QianMa <vanqianma@qq.com>
2023-02-18 14:38:48 +0800

    loss

--- libcity/model/traffic_speed_prediction/TGCN.py
+++ libcity/model/traffic_speed_prediction/TGCN.py
@@ -233,7 +233,7 @@ class TGCN(AbstractTrafficStateModel):
         y_true = self._scaler.inverse_transform(labels[..., :self.output_dim])
         y_predicted = self._scaler.inverse_transform(y_predicted[..., :self.output_dim])
 
-        loss = torch.mean(torch.norm(y_true - y_predicted) ** 2 / 2) + lam * lreg
+        loss = torch.mean(torch.norm(y_true - y_predicted) ** 2 / 2) #+ lam * lreg
         loss /= y_predicted.numel()
         # return loss.masked_mae_torch(y_predicted, y_true, 0)
         return loss
